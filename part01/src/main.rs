c242706ab01a0a987224c7df58079b0c50739ce7
Linus Walker <w000alker@gmail.com>
2023-03-04 18:22:16 +0630

    Text in Display

--- part01/src/main.rs
+++ part01/src/main.rs
@@ -3,7 +3,18 @@
 
 extern crate panic_halt;
 
+static TEXT: &[u8] = b"We're Fucked up!";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    let vga_buff = 0xb8000 as *mut u8;
+
+    for (idk, &byte) in TEXT.iter().enumerate() {
+        unsafe {
+            *vga_buff.offset(idk as isize * 2) = byte;
+            *vga_buff.offset(idk as isize * 2 + 1) = 0x0b;
+        }
+    }
+
     loop {}
 }
